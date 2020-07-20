de1f8204a9c18233ffc84ba6b1f083294de8d749
Babyonline <babyonline3000@gmail.com>
2020-07-20 19:46:38 +0300

    add models and edit migrate

---
+++ common/models/ReservedRoom.php
@@ -0,0 +1,73 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * ReservedRoom model
+ *
+ * @property integer $id
+ * @property integer $rooms_id
+ * @property string $username
+ * @property string $phone
+ * @property string $date_to_reserved
+ * @property string $date_out_reserved
+ * @property string $created_at
+ */
+class ReservedRoom extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%reserved_room}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'phone', 'date_to_reserved', 'created_at'], 'required'],
+            [['username', 'phone', 'date_to_reserved', 'date_out_reserved', 'created_at'], 'string'],
+            [['username', 'phone', 'date_to_reserved', 'date_out_reserved', 'created_at'], 'trim'],
+            [['rooms_id'], 'exist', 'skipOnError' => true, 'targetClass' => Rooms::class, 'targetAttribute' => ['rooms_id' => 'id']],
+            [['date_out_reserved'], 'default', 'value' => null],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'                => 'ID',
+            'rooms_id'          => 'ID номера',
+            'username'          => 'Имя пользователя',
+            'phone'             => 'Телефон',
+            'date_to_reserved'  => 'Дата заезда',
+            'date_out_reserved' => 'Дата выезда',
+            'created_at'        => 'Дата создания',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => \yii\behaviors\TimestampBehavior::class,
+                'attributes' => [
+                    self::EVENT_BEFORE_INSERT => ['created_at'],
+                ],
+                'value' => date('Y-m-d H:i:s'),
+            ],
+        ];
+    }
+}
---
+++ common/models/Rooms.php
@@ -0,0 +1,47 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * Rooms model
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ */
+class Rooms extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%rooms}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name', 'description'], 'string'],
+            [['name', 'description'], 'trim'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'                   => 'ID',
+            'name'                 => 'Название',
+            'description'          => 'Описание',
+        ];
+    }
+}
